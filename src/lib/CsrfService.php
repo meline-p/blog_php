280cbef95c17b40a833cfc78e88ffdffa299dc62
meline-p <meline.pischedda@gmail.com>
2024-02-17 14:20:03 +0100

    add documentation for csrf service

--- src/lib/CsrfService.php
+++ src/lib/CsrfService.php
@@ -2,8 +2,18 @@
 
 namespace App\lib;
 
+/**
+ * Class representing CSRF Token.
+ */
 class CsrfService
 {
+    /**
+     * Generates and returns a CSRF token.
+     * If the request method is GET, generates a new token and stores it in the session.
+     * Returns the current CSRF token, which can be used in forms.
+     *
+     * @return string The CSRF token.
+     */
     public static function getCsrfToken()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'GET') {
@@ -14,6 +24,14 @@ class CsrfService
         return $_SESSION['csrf_token'];
     }
 
+    /**
+     * Redirects to an error page if the CSRF token is invalid.
+     * If the request method is GET, the function does nothing.
+     * If the request method is POST, it checks the presence and validity of the CSRF token.
+     * If the token is invalid, the user is redirected to a CSRF error page.
+     *
+     * @return null
+     */
     public static function redirectIfInvalid()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'GET') {
