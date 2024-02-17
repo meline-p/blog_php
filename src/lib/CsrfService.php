3c02bcb3b9b0fdddee797476df708fb3334f80e8
meline-p <meline.pischedda@gmail.com>
2024-02-17 14:22:45 +0100

    add return null statement

--- src/lib/CsrfService.php
+++ src/lib/CsrfService.php
@@ -35,12 +35,14 @@ class CsrfService
     public static function redirectIfInvalid()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-            return;
+            return null;
         }
 
         if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
             header('location: /csrf_invalid');
             exit;
         }
+
+        return null;
     }
 }
